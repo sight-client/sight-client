2234b8296da4086990ef7d6341d0b9178f183fc3
sight-client <porphirik88@gmail.com>
2026-01-09 13:56:10 +0300

    tech commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
 // define an environment variable that tells Cesium the base URL to load static files
-(window as Record<string, any>)['CESIUM_BASE_URL'] = '/assets/cesium/';
+(window as Record<string, any>)['CESIUM_BASE_URL'] = '/sight-client/assets/cesium/';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
