cec66dc509e9c6d13513d593328b6422054784a2
Evgeniy <porphirik88@gmail.com>
2026-09-16 14:31:41 +0300

    tech commit

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
 // define an environment variable that tells Cesium the base URL to load static files
-(window as Record<string, any>)['CESIUM_BASE_URL'] = '/assets/cesium/';
-// При этом в angular.json в конфиге сборки д/б: "baseHref": "/sight-client/" - имя репозитория на gitgub (для github pages),
+(window as Record<string, any>)['CESIUM_BASE_URL'] = '/sight-client/assets/cesium/';
+// При этом в angular.json во всех конфигах сборки д/б: "baseHref": "/sight-client/" - имя репозитория на gitgub (для github pages),
 // а в его конфиге ассетов д/б:
 //               {
 //                 "glob": "**/*",
